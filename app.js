ac642a608ac07b59b7766e70ac91daf4c2852b55
firkax-new-desk <157710697+Firkax69@users.noreply.github.com>
2024-09-11 12:49:01 +0100

    initial set up

--- app.js
+++ app.js
@@ -2,6 +2,18 @@ const express = require('express');
 const app = express();
 const path = require("node:path");
 
+const messages = [
+    {
+      text: "Hi there!",
+      user: "Amando",
+      added: new Date()
+    },
+    {
+      text: "Hello World!",
+      user: "Charles",
+      added: new Date()
+    }
+  ];
 
 // app.get("/", (req, res) => res.send("Hello, world!"));
 
