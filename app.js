c50ea4f13c877091ac3556bc87ddba444bf86fbf
firkax-new-desk <157710697+Firkax69@users.noreply.github.com>
2024-09-11 15:24:04 +0100

    fix: form user and message dispaly

--- app.js
+++ app.js
@@ -25,6 +25,28 @@ app.get('/new', (req, res) => {
   res.render('form');   // Render the 'form.ejs' template
 })
 
+// MIDDLEWARE: Before handling the form data, make sure that your Express app can parse POST request bodies. 
+app.use(express.urlencoded({extended: true}));
+
+
+
+// Route handler for form submission (POST request)
+app.post('/new', (req, res) => {
+  const {author, message} = req.body;
+
+  messages.push({
+    text: message,
+    user: author,
+    added: new Date()
+  });
+
+  
+  // Redirect to the home page to display the new message
+  res.redirect('/');
+})
+
+
+
 // Set the view engine (assuming you are using EJS)
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
